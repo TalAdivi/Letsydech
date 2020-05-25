10da9ef4e24772e571d81181043e9e19c2da3ca6
Fadi Atamny <fadiatamny@gmail.com>
2020-05-25 13:10:24 +0300

    standard layout of homepage

--- src/pages/home/cards/index.tsx
+++ src/pages/home/cards/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Card from './card';
 
-const Cards = () => {
+import styles from './cards.module.scss';
+
+const Cards = ({ title, data }: { title: string, data: Array<{ icon: string, text: string }> }) => {
   return (
-    <Grid container spacing={2} justify="center">
-      <Grid item xs={6}>
-        <Card icon='' text='test' />
-      </Grid>
-      <Grid item xs={6}>  
-        <Card icon='' text='test' />
-      </Grid>
-      <Grid item xs={6}>
-        <Card icon='' text='test' />
-      </Grid>
+    <Grid container justify="center">
+      {title !== '' ?
+        <Grid className={styles.content} container spacing={2} xs={10} justify='center'>
+          <Grid item xs={2}>
+            <Paper className='item'>{title}</Paper>
+          </Grid>
+        </Grid>
+        : null}
+      {data ?
+        <Grid className={styles.content} container spacing={2} xs={10} justify='center'>
+          {data.map((elem: { icon: string, text: string }): any => (
+            <Grid item xs={4} key={elem.icon + elem.text}>
+              <Card icon={elem.icon} text={elem.text} />
+            </Grid>
+          ))}
+        </Grid>
+        : null}
     </Grid>
   );
 };
