b68d14c03296d150ba6a1c489759ef313711af75
Fadi Atamny <fadiatamny@gmail.com>
2020-06-02 05:00:52 +0300

    implemented basic image gallery

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Navbar from '../../shared/components/navbar';
 
 const Gallery = (props: any): any => {
+  const images: Array<{ url: string, title: string, cols: number }> = [];
+
   return (
     <div>
-      test
+      <Navbar navigation={props.navigation} />
+      <GridList cellHeight={160} cols={5}>
+        {images.map((tile) => (
+          <GridListTile key={tile.url} cols={tile.cols || 1}>
+            <img src={tile.url} alt={tile.title} />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 };
