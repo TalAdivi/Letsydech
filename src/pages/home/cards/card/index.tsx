d081ffeb2fac27248ebfae06b2457c4112624317
Fadi Atamny <fadiatamny@gmail.com>
2020-08-17 03:18:16 +0300

    fixed supporters

--- src/pages/home/cards/card/index.tsx
+++ src/pages/home/cards/card/index.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { Card, CardContent, Typography, CardMedia, Grid } from '@material-ui/core';
 import styles from './card.module.scss';
 
 const SingleCard = ({ icon, text }: { icon: string, text: string }) => {
   return (
-
-    // <>
-    <Grid item xl={"auto"} alignContent="space-around">
-      <CardMedia
-          className={styles.myCardMedia}
-          component="img"
-          alt={text}
-          height="550 !important"
-          width= "370 !important"
-          image= {icon}
-          title={text}
-        />
-
-      <CardContent >
-        <Typography gutterBottom variant="subtitle1" component="h6" align="center">
-          {text}
-        </Typography>
-      </CardContent>
-    </Grid>
-
-    // {/* </> */}
+    <div className={styles.container}>
+      <div className="card">
+        <div className="visual">
+          <img className={styles.icon} src={icon} alt={text} />
+        </div>
+        <p className={styles.p}>{text}</p>
+      </div>
+    </div>
   );
 };
 
